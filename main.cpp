1e75073454b9e538bb7d8ae37c1b9a4f39286774
cheoljun99 <cheoljun99@naver.com>
2025-05-06 23:36:55 +0900

    [feat] : 2025.05.06 버전 업데이트

--- main.cpp
+++ main.cpp
@@ -42,11 +42,11 @@ typedef struct iface {
     Mac mac;
 }IFACE;
 
-#define PUBLIC_IFACE_NAME_HARDCODE "wlo1"
-#define PRIVATE_IFACE_NAME_HARDCODE "enx8cb0e9e8cf94"
-#define PUBLIC_GATEWAY_IP_HARDCODE "192.168.0.1"
-#define PRIVATE_GATEWAY_IP_HARDCODE "10.10.10.111"
-#define PAKE_IP_HARDCODE "192.168.0.111"
+#define INTERNET_IFACE_NAME_HARDCODE "wlo1"
+#define ISOLATED_IFACE_NAME_HARDCODE "enx8cb0e9e8cf94"
+#define INTERNET_GATEWAY_IP_HARDCODE "192.168.0.1"
+#define ISOLATED_TARGET_IP_HARDCODE "10.10.10.111"
+#define INTERNET_PAKE_IP_HARDCODE "192.168.0.111"
 
 IFACE* get_iface_IP_and_MAC(const char* ifname) {
     printf("Get interface IP and MAC, and disable offload features\n");
@@ -168,17 +168,17 @@ Mac get_gateway_mac_using_ARP_request(IFACE * iface, Ip gateway_ip, pcap_t* hand
 }
 
 
-void monitor_iface_and_send_to_other_iface_loop_thread_func( pcap_t*private_iface_handle,
-	 pcap_t*public_iface_handle,
-	  IFACE * private_iface,
-	  IFACE * public_iface,
-	  IFACE * private_network_gateway,
-	  IFACE * public_network_gateway,
-	  Ip fake_ip) {
+void monitor_iface_and_send_to_other_iface_loop_thread_func( pcap_t*isolated_iface_handle,
+	 pcap_t*internet_iface_handle,
+	  IFACE * isolated_iface,
+	  IFACE * internet_iface,
+	  IFACE * isolated_network_target,
+	  IFACE * internet_network_gateway,
+	  Ip internet_network_fake_ip) {
 	struct pcap_pkthdr* header;
 	const u_char* packet;
 	while (true) {
-		int res = pcap_next_ex(private_iface_handle, &header, &packet);
+		int res = pcap_next_ex(isolated_iface_handle, &header, &packet);
 		if (res == 0)
 			continue; // 타임아웃 발생 시 루프 계속
 		else if (res == -1 || res == -2) {
@@ -195,14 +195,14 @@ void monitor_iface_and_send_to_other_iface_loop_thread_func( pcap_t*private_ifac
 		EthHdr* ethPacket = (EthHdr*)packet;
 		IpHdr* iphdr = (IpHdr*)(packet + sizeof(EthHdr));
 		
-		if (ethPacket->smac() == private_network_gateway->mac&&ethPacket->type() ==EthHdr::Ip4&&iphdr->dip()!=private_network_gateway->ip){
+		if (ethPacket->smac() == isolated_network_target->mac&&ethPacket->type() ==EthHdr::Ip4&&iphdr->sip()==isolated_network_target->ip){
 
 			// 1. MAC 주소 수정
-			ethPacket->smac_ = public_iface->mac;
-			ethPacket->dmac_ = public_network_gateway->mac;
+			ethPacket->smac_ = internet_iface->mac;
+			ethPacket->dmac_ = internet_network_gateway->mac;
 
-			// 2. IP 주소 수정 (src ip인 private_network_gateway_ip를 fake_ip로 변경)
-			iphdr->sip_ = htonl(fake_ip);                    // src IP
+			// 2. IP 주소 수정 (src ip인 isolated_network_target_ip를 fake_ip로 변경)
+			iphdr->sip_ = htonl(internet_network_fake_ip);                    // src IP
 	
 			// 3. IP 체크섬 재계산
 			iphdr->sum = 0;
@@ -251,9 +251,9 @@ void monitor_iface_and_send_to_other_iface_loop_thread_func( pcap_t*private_ifac
 
 				
 			std::lock_guard<std::mutex> lock(send_lock);
-			int res = pcap_sendpacket(public_iface_handle, packet, header->len);
+			int res = pcap_sendpacket(internet_iface_handle, packet, header->len);
 			if (res != 0) {
-				fprintf(stderr, "pcap_sendpacket return %d error=%s header->len=%d \n", res, pcap_geterr(public_iface_handle),header->len);
+				fprintf(stderr, "pcap_sendpacket return %d error=%s header->len=%d \n", res, pcap_geterr(internet_iface_handle),header->len);
 			}
 		}
 		
@@ -261,20 +261,25 @@ void monitor_iface_and_send_to_other_iface_loop_thread_func( pcap_t*private_ifac
 }
 
 
-void monitor_arp_and_send_arp_loop_thread_func( pcap_t*public_iface_handle, 
-	IFACE * public_iface,
-	IFACE * public_network_gateway,
-	pcap_t* private_iface_handle, 
-	IFACE * private_iface,IFACE * private_network_gateway,Ip fake_ip) {
+void monitor_arp_and_send_arp_loop_thread_func( pcap_t*internet_iface_handle, 
+	IFACE * internet_iface,
+	IFACE * internet_network_gateway,
+	pcap_t* isolated_iface_handle, 
+	IFACE * isolated_iface,IFACE * isolated_network_target,Ip internet_network_fake_ip) {
 	struct pcap_pkthdr* header;
 	const u_char* packet;
 	while (true) {
-		int res = pcap_next_ex(public_iface_handle, &header, &packet);
+		int res = pcap_next_ex(internet_iface_handle, &header, &packet);
 		if (res == 0)
 			continue; // 타임아웃 발생 시 루프 계속
 		else if (res == -1 || res == -2) {
 			printf("Error occurred while capturing packets\n");
 			break;
+		}
+		// 캡처 부족 여부 체크
+		if (header->caplen < header->len) {
+			printf("[DEBUG] WARNNING 캡처된 길이가 부족합니다: caplen=%d, len=%d\n",
+				   header->caplen, header->len);
 		}
 	
 		EthArpPacket* ethArpPacket = (EthArpPacket*)packet;
@@ -283,22 +288,22 @@ void monitor_arp_and_send_arp_loop_thread_func( pcap_t*public_iface_handle,
 				printf("ARP request detected from gateway. Reapplying ARP response...\n");
 				//센더에게 타겟맥이 내 맥이라고 속이기.
 				EthArpPacket response_packet;
-				response_packet.eth_.dmac_ = public_network_gateway->mac;
-				response_packet.eth_.smac_ = public_iface->mac;
+				response_packet.eth_.dmac_ = internet_network_gateway->mac;
+				response_packet.eth_.smac_ = internet_iface->mac;
 				response_packet.eth_.type_ = htons(EthHdr::Arp);
 				response_packet.arp_.hrd_ = htons(ArpHdr::ETHER);
 				response_packet.arp_.pro_ = htons(EthHdr::Ip4);
 				response_packet.arp_.hln_ = Mac::SIZE;
 				response_packet.arp_.pln_ = Ip::SIZE;
 				response_packet.arp_.op_ = htons(ArpHdr::Reply);
-				response_packet.arp_.smac_ = public_iface->mac;
-				response_packet.arp_.sip_ = htonl(fake_ip);
-				response_packet.arp_.tmac_ = public_network_gateway->mac;
-				response_packet.arp_.tip_ = htonl(public_network_gateway->ip);
+				response_packet.arp_.smac_ = internet_iface->mac;
+				response_packet.arp_.sip_ = htonl(internet_network_fake_ip);
+				response_packet.arp_.tmac_ = internet_network_gateway->mac;
+				response_packet.arp_.tip_ = htonl(internet_network_gateway->ip);
 				std::lock_guard<std::mutex> lock(send_lock);
-				int res = pcap_sendpacket(public_iface_handle, reinterpret_cast<const u_char*>(&response_packet), sizeof(EthArpPacket));
+				int res = pcap_sendpacket(internet_iface_handle, reinterpret_cast<const u_char*>(&response_packet), sizeof(EthArpPacket));
 				if (res != 0) {
-					fprintf(stderr, "pcap_sendpacket return %d error=%s header->len=%d \n", res, pcap_geterr(public_iface_handle),header->len);
+					fprintf(stderr, "pcap_sendpacket return %d error=%s header->len=%d \n", res, pcap_geterr(internet_iface_handle),header->len);
 					exit(-1);
 				}
 
@@ -306,13 +311,13 @@ void monitor_arp_and_send_arp_loop_thread_func( pcap_t*public_iface_handle,
 		else{
 			EthHdr* ethPacket = (EthHdr*)packet;
 			IpHdr* iphdr = (IpHdr*)(packet + sizeof(EthHdr));
-			if (ethPacket->smac() == public_network_gateway->mac&&ethPacket->type() ==EthHdr::Ip4&&iphdr->dip()==fake_ip){
+			if (ethPacket->smac() == internet_network_gateway->mac&&ethPacket->type() ==EthHdr::Ip4&&iphdr->dip()==internet_network_fake_ip){
 				//1. mac 수정
-				ethPacket->smac_ = private_iface->mac;
-				ethPacket->dmac_ = private_network_gateway->mac;
+				ethPacket->smac_ = isolated_iface->mac;
+				ethPacket->dmac_ = isolated_network_target->mac;
 
-				// 2. IP 주소 수정 (dest ip인 fake_ip를 private_network_gateway_ip로 변경)
-				iphdr->dip_ = htonl(private_network_gateway->ip);// dest IP
+				// 2. IP 주소 수정 (dest ip인 fake_ip를 isolated_network_target_ip로 변경)
+				iphdr->dip_ = htonl(isolated_network_target->ip);// dest IP
 		
 				// 3. IP 체크섬 재계산
 				iphdr->sum = 0;
@@ -357,9 +362,9 @@ void monitor_arp_and_send_arp_loop_thread_func( pcap_t*public_iface_handle,
 					udph->sum_ = CalcUdpChecksum(iphdr, udph, payload, payload_len);
 				}
 				std::lock_guard<std::mutex> lock(send_lock);		
-				int res = pcap_sendpacket(private_iface_handle, packet, header->len);
+				int res = pcap_sendpacket(isolated_iface_handle, packet, header->len);
 				if (res != 0) {
-					fprintf(stderr, "pcap_sendpacket return %d error=%s header->len=%d \n", res, pcap_geterr(private_iface_handle),header->len);
+					fprintf(stderr, "pcap_sendpacket return %d error=%s header->len=%d \n", res, pcap_geterr(isolated_iface_handle),header->len);
 				}
 			}
 		}
@@ -373,9 +378,9 @@ void monitor_arp_and_send_arp_loop_thread_func( pcap_t*public_iface_handle,
 
 int main() {
 
-	printf("Enter name of public network interface : ");
+	printf("Enter name of internet network interface : ");
 	char buf[100];
-	char public_iface_name[100];
+	char internet_iface_name[100];
 	int ch;
 	if (fgets(buf, sizeof(buf), stdin) == NULL) {
 		printf("Input error\n");
@@ -387,13 +392,13 @@ int main() {
 	else{
 		while ((ch = getchar()) != '\n' && ch != EOF);
 	}
-	if(sscanf(buf,"%99s", public_iface_name)!=1){
+	if(sscanf(buf,"%99s", internet_iface_name)!=1){
 		printf("Invalid number\n");
 		return -1;
 	}
 
-	printf("Enter name of private network interface : ");
-	char private_iface_name[100];
+	printf("Enter name of isolated network interface : ");
+	char isolated_iface_name[100];
 	if (fgets(buf, sizeof(buf), stdin) == NULL) {
 		printf("Input error\n");
 		return -1;
@@ -404,41 +409,41 @@ int main() {
 	else{
 		while ((ch = getchar()) != '\n' && ch != EOF);
 	}
-	if(sscanf(buf,"%99s", private_iface_name)!=1){
+	if(sscanf(buf,"%99s", isolated_iface_name)!=1){
 		printf("Invalid number\n");
 		return -1;
 	}
 
 
 	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* public_iface_handle = pcap_open_live(public_iface_name, BUFSIZ, 1, 1, errbuf); // 타임아웃을 1000ms로 설정
-	if (public_iface_handle == nullptr) {
-		fprintf(stderr, "couldn't open device %s(%s)\n", public_iface_name, errbuf);
+	pcap_t* internet_iface_handle = pcap_open_live(internet_iface_name, BUFSIZ, 1, 1, errbuf); // 타임아웃을 1000ms로 설정
+	if (internet_iface_handle == nullptr) {
+		fprintf(stderr, "couldn't open device %s(%s)\n", internet_iface_name, errbuf);
 		return -1;
 	}
-	pcap_t* private_iface_handle = pcap_open_live(private_iface_name, BUFSIZ, 1, 1, errbuf); // 타임아웃을 1000ms로 설정
-	if (private_iface_handle == nullptr) {
-		fprintf(stderr, "couldn't open device %s(%s)\n", private_iface_name, errbuf);
+	pcap_t* isolated_iface_handle = pcap_open_live(isolated_iface_name, BUFSIZ, 1, 1, errbuf); // 타임아웃을 1000ms로 설정
+	if (isolated_iface_handle == nullptr) {
+		fprintf(stderr, "couldn't open device %s(%s)\n", isolated_iface_name, errbuf);
 		return -1;
 	}
 
-	IFACE * public_iface = get_iface_IP_and_MAC(public_iface_name);
-    if (!public_iface) {
-        fprintf(stderr, "Failed to get public interface's IP and MAC address\n");
+	IFACE * internet_iface = get_iface_IP_and_MAC(internet_iface_name);
+    if (!internet_iface) {
+        fprintf(stderr, "Failed to get internet interface's IP and MAC address\n");
         return -1;
     }
 
-	IFACE * private_iface = get_iface_IP_and_MAC(private_iface_name);
-    if (!private_iface) {
-        fprintf(stderr, "Failed to get private interface's IP and MAC address\n");
+	IFACE * isolated_iface = get_iface_IP_and_MAC(isolated_iface_name);
+    if (!isolated_iface) {
+        fprintf(stderr, "Failed to get isolated interface's IP and MAC address\n");
         return -1;
     }
 
 
 	
 
-	printf("Enter ip of public network gateway : ");
-	char public_network_gateway_ip[100];
+	printf("Enter gateway ip of internet network : ");
+	char internet_network_gateway_ip[100];
 	if (fgets(buf, sizeof(buf), stdin) == NULL) {
 		printf("Input error\n");
 		return -1;
@@ -449,14 +454,14 @@ int main() {
 	else{
 		while ((ch = getchar()) != '\n' && ch != EOF);
 	}
-	if(sscanf(buf,"%99s", public_network_gateway_ip)!=1){
+	if(sscanf(buf,"%99s", internet_network_gateway_ip)!=1){
 		printf("Invalid number\n");
 		return -1;
 	}
 
 
-	printf("Enter ip of private network gateway : ");
-	char private_network_gateway_ip[100];
+	printf("Enter target ip of isolated network : ");
+	char isolated_network_target_ip[100];
 	if (fgets(buf, sizeof(buf), stdin) == NULL) {
 		printf("Input error\n");
 		return -1;
@@ -467,13 +472,13 @@ int main() {
 	else{
 		while ((ch = getchar()) != '\n' && ch != EOF);
 	}
-	if(sscanf(buf,"%99s", private_network_gateway_ip)!=1){
+	if(sscanf(buf,"%99s", isolated_network_target_ip)!=1){
 		printf("Invalid number\n");
 		return -1;
 	}
 
-	printf("Enter fake ip of public network (== arp spoofing ip) : ");
-	char fake_ip[100];
+	printf("Enter fake ip of internet network (== arp spoofing ip) : ");
+	char internet_network_fake_ip[100];
 	if (fgets(buf, sizeof(buf), stdin) == NULL) {
 		printf("Input error\n");
 		return -1;
@@ -484,36 +489,36 @@ int main() {
 	else{
 		while ((ch = getchar()) != '\n' && ch != EOF);
 	}
-	if(sscanf(buf,"%99s", fake_ip)!=1){
+	if(sscanf(buf,"%99s", internet_network_fake_ip)!=1){
 		printf("Invalid number\n");
 		return -1;
 	}
 
 
 
-	IFACE public_network_gateway = {Ip(public_network_gateway_ip),
-		get_gateway_mac_using_ARP_request(public_iface,Ip(public_network_gateway_ip),public_iface_handle)};
-	IFACE private_network_gateway = {Ip(private_network_gateway_ip),
-		get_gateway_mac_using_ARP_request(private_iface,Ip(private_network_gateway_ip),private_iface_handle)};
+	IFACE internet_network_gateway = {Ip(internet_network_gateway_ip),
+		get_gateway_mac_using_ARP_request(internet_iface,Ip(internet_network_gateway_ip),internet_iface_handle)};
+	IFACE isolated_network_target = {Ip(isolated_network_target_ip),
+		get_gateway_mac_using_ARP_request(isolated_iface,Ip(isolated_network_target_ip),isolated_iface_handle)};
 	
 	thread monitor_iface_and_send_to_other_iface_loop_thread(monitor_iface_and_send_to_other_iface_loop_thread_func,
-		 private_iface_handle,
-		 public_iface_handle,
-		 private_iface,public_iface,
-		 &private_network_gateway,
-		 &public_network_gateway,
-		 Ip(fake_ip));
+		 isolated_iface_handle,
+		 internet_iface_handle,
+		 isolated_iface,internet_iface,
+		 &isolated_network_target,
+		 &internet_network_gateway,
+		 Ip(internet_network_fake_ip));
 		 
 	thread monitor_arp_and_send_arp_loop_thread(monitor_arp_and_send_arp_loop_thread_func,
-		public_iface_handle,public_iface,
-		&public_network_gateway,private_iface_handle,
-		private_iface,
-		&private_network_gateway,
-		Ip(fake_ip));
+		internet_iface_handle,internet_iface,
+		&internet_network_gateway,isolated_iface_handle,
+		isolated_iface,
+		&isolated_network_target,
+		Ip(internet_network_fake_ip));
 	
 	monitor_iface_and_send_to_other_iface_loop_thread.join();
 	monitor_arp_and_send_arp_loop_thread.join();
 
-	pcap_close(public_iface_handle);
-	pcap_close(private_iface_handle);
+	pcap_close(internet_iface_handle);
+	pcap_close(isolated_iface_handle);
 }
